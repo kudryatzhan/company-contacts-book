732903752a8d48103dae2bd89f04ed356c6fa8f4
Kudryatzhan Arziyev <karziyev@softomate.com>
2020-09-02 09:49:40 +0600

    Setup companies table view controller

---
+++ CompanyContacts/Supporting Files/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  CompanyContacts
+//
+//  Created by Kudryatzhan Arziyev on 9/2/20.
+//  Copyright © 2020 Kudryatzhan Arziyev. All rights reserved.
+//
+
+struct K {
+    static let cellReuseIdentifier = "UITableViewCell"
+}
+
--- CompanyContacts/ViewController.swift
+++ CompanyContacts/ViewController.swift
@@ -8,14 +8,18 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
 
+    // MARK: - View lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .white
         setupNavigationBar()
+        setupTableView()
     }
+    
+    // MARK: - Helper methods
 
     fileprivate func setupNavigationBar() {
         // Navigation item
@@ -30,7 +34,8 @@ class ViewController: UIViewController {
             let navBarAppearance = UINavigationBarAppearance()
             navBarAppearance.configureWithOpaqueBackground()
             navBarAppearance.largeTitleTextAttributes = [.foregroundColor: UIColor.white]
-            navBarAppearance.backgroundColor = .lightRedNavigationBarColor
+            navBarAppearance.titleTextAttributes = [.foregroundColor: UIColor.white]
+            navBarAppearance.backgroundColor = .navigationBarColor
             
             navigationController?.navigationBar.prefersLargeTitles = true
             navigationController?.navigationBar.standardAppearance = navBarAppearance
@@ -38,8 +43,52 @@ class ViewController: UIViewController {
         }
     }
     
+    fileprivate func setupTableView() {
+        tableView.backgroundColor = .tableViewColor
+        tableView.tableFooterView = UIView() // Blank footer
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: K.cellReuseIdentifier)
+        tableView.separatorColor = .white
+    }
+    
+    // MARK: - IBActions
+    
     @objc fileprivate func addButtonTapped(_ sender: UIBarButtonItem) {
         print(#function)
     }
 }
 
+// MARK: - UITableViewDataSource
+
+extension ViewController {
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 8
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: K.cellReuseIdentifier, for: indexPath)
+        
+        cell.backgroundColor = .cellColor
+        cell.textLabel?.text = "\(indexPath.row)"
+        cell.textLabel?.textColor = .white
+        cell.textLabel?.font = .boldSystemFont(ofSize: 16)
+        
+        return cell
+    }
+}
+
+// MARK: - UITableviewDelegate
+
+extension ViewController {
+    
+    override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let view = UIView()
+        view.backgroundColor = .tableViewHeaderColor
+        return view
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 50
+    }
+}
